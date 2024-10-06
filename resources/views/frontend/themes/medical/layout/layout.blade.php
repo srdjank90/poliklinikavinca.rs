57833953168be8a823a08bcbae930aab30003061
Srdjan Kordic <srdjank90@gmail.com>
2024-10-06 21:36:35 +0200

    Fixed thing

--- resources/views/frontend/themes/medical/layout/layout.blade.php
+++ resources/views/frontend/themes/medical/layout/layout.blade.php
@@ -35,7 +35,7 @@
     <meta name="robots" content="index, follow">
 
     <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16659687689"></script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-V48ETQGX1Y"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
 
@@ -43,32 +43,7 @@
             dataLayer.push(arguments);
         }
         gtag('js', new Date());
-
-        gtag('config', 'AW-16659687689');
-    </script>
-
-    <!-- Meta Pixel Code -->
-    <script>
-        ! function(f, b, e, v, n, t, s) {
-            if (f.fbq) return;
-            n = f.fbq = function() {
-                n.callMethod ?
-                    n.callMethod.apply(n, arguments) : n.queue.push(arguments)
-            };
-            if (!f._fbq) f._fbq = n;
-            n.push = n;
-            n.loaded = !0;
-            n.version = '2.0';
-            n.queue = [];
-            t = b.createElement(e);
-            t.async = !0;
-            t.src = v;
-            s = b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t, s)
-        }(window, document, 'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-        fbq('init', '1089573011603937');
-        fbq('track', 'PageView');
+        gtag('config', 'G-V48ETQGX1Y');
     </script>
 
 </head>
